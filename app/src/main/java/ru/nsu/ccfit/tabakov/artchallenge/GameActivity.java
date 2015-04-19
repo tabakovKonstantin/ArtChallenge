5e87efb46219425ea301fb41282604dbac28d91f
konstantinTabakov <konkov90@gmail.com>
2015-04-19 17:39:57 +0600

    причесал код, сделал логи

--- app/src/main/java/ru/nsu/ccfit/tabakov/artchallenge/GameActivity.java
+++ app/src/main/java/ru/nsu/ccfit/tabakov/artchallenge/GameActivity.java
@@ -16,11 +16,7 @@ import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Locale;
-import java.util.Map;
-import java.util.Random;
-import java.util.Set;
 
 import retrofit.Callback;
 import retrofit.RestAdapter;
@@ -33,12 +29,16 @@ import retrofit.client.Response;
 public class GameActivity  extends Activity implements View.OnClickListener {
     private static final String BASE_URL = "http://artchallenge.me";
     private static final int MAX_ANSWER = 10;
-    private ArrayList<Button> setButtons = new ArrayList<Button>();
-    private int numTrueButton;
-    private int idPainter;
+
     private int countTrueAnswer = 0;
+    private int numButtonTrueAnswer;
+    private int idPainter;
+    private String picURl;
+
     private ProgressBar progressBar;
     private ImageView imageViewPicture;
+    private ArrayList<Button> setButtons = new ArrayList<Button>();
+
     private IPainterApi restApi;
     private Translation translation;
     private ArrayList<Integer> setPainters = new ArrayList<Integer>(Arrays.asList(2,3,9,16,17,21,30,36,49,53,57,60,61,69,77,84,94,96));
@@ -49,25 +49,7 @@ public class GameActivity  extends Activity implements View.OnClickListener {
         setContentView(R.layout.game);
         initComponents();
         setListener(this);
-        setClickable(false);
-
-
-        String locale = Locale.getDefault().getLanguage();
-        Log.i("qwer", "locale " + locale);
-        restApi.getTranslation(locale, new Callback<Translation>() {
-            @Override
-            public void success(Translation translation, Response response) {
-                setButtonText(translation);
-                setNewPicture(idPainter);
-                setTranslation(translation);
-            }
-
-            @Override
-            public void failure(RetrofitError error) {
-
-            }
-        });
-
+        startGame();
     }
 
     @Override
@@ -90,7 +72,6 @@ public class GameActivity  extends Activity implements View.OnClickListener {
                 break;
             }
             case R.id.imageViewmPicture: {
-                Log.i("rewq", "dhdffgfhg");
                 break;
             }
         }
@@ -115,6 +96,10 @@ public class GameActivity  extends Activity implements View.OnClickListener {
         progressBar.setMax(MAX_ANSWER);
     }
 
+    public void setTranslation(Translation translation) {
+        this.translation = translation;
+    }
+
     private void setListener(View.OnClickListener listener) {
         for(Button button : setButtons) {
             button.setOnClickListener(listener);
@@ -128,66 +113,23 @@ public class GameActivity  extends Activity implements View.OnClickListener {
         }
     }
 
-    private void checkTruePainter(int numAnswer) {
-        if (numAnswer == numTrueButton) {
-            countTrueAnswer ++;
-            Toast.makeText(this, translation.getRandGoodPhrases(), Toast.LENGTH_SHORT).show();
-            Log.i("qwer", "Выбран правильный ответ");
+    private void setButtonColorText(int numButtonTrueAnswer, int numButtonFalseAnswer) {
+        if(numButtonTrueAnswer == numButtonFalseAnswer) {
+            setButtons.get(numButtonTrueAnswer - 1).setTextColor(Color.GREEN);
         } else {
-            countTrueAnswer = 0;
-            Toast.makeText(this, translation.getRandBadPhrases(), Toast.LENGTH_SHORT).show();
-            Log.i("qwer", "Выбран не правильный ответ");
-        }
-
-        progressBar.setProgress(countTrueAnswer);
-        if(countTrueAnswer != MAX_ANSWER) {
-            setBackgroundColor(numTrueButton, numAnswer);
-            setButtonText(translation);
-            setNewPicture(idPainter);
-            Log.i("qwer", "Художник " + String.valueOf(idPainter));
-        } else {
-            Log.i("rewq", "WIN ");
-        }
-
-    }
-
-    private void setBackgroundColor(int trueAnswerNum, int falseAnswerNum) {
-        if(trueAnswerNum == falseAnswerNum) {
-            setButtons.get(trueAnswerNum - 1).setTextColor(Color.GREEN);
-        } else {
-            setButtons.get(trueAnswerNum - 1).setTextColor(Color.GREEN);
-            setButtons.get(falseAnswerNum - 1).setTextColor(Color.RED);
+            setButtons.get(numButtonTrueAnswer - 1).setTextColor(Color.GREEN);
+            setButtons.get(numButtonFalseAnswer - 1).setTextColor(Color.RED);
         }
     }
 
-    private void resetBackgroundColor() {
+    private void resetButtonColorText() {
         for(Button button : setButtons) {
             button.setTextColor(Color.BLACK);
         }
-        Log.i("rewq","vizov");
     }
-
-
-
-
-    private void setButtonText(Translation translation) {
-
-        ArrayList<Integer> uniqSet = RandomMath.randomSet(1, setPainters.size(), setButtons.size());
-        numTrueButton = RandomMath.randomInt(1, 4);
-        idPainter = uniqSet.get(numTrueButton - 1);
-
-        for (int i = 0; i < uniqSet.size(); i++) {
-            String textButton = translation.getPainters().get(uniqSet.get(i));
-            setButtons.get(i).setText(textButton);
-        }
-        Log.i("rewq", "правильная кнопка " + String.valueOf(numTrueButton) + "  " + translation.getPainters().get(idPainter));
-    }
-
-
 
     private String createUrlForPicture(int idPainter, int pictureQuantity) {
         int numPicture = RandomMath.randomInt(1, pictureQuantity);
-        Log.i("qwer", "картина художника " + String.valueOf(numPicture));
         return BASE_URL
                 .concat("/painters/")
                 .concat(String.valueOf(idPainter))
@@ -196,32 +138,85 @@ public class GameActivity  extends Activity implements View.OnClickListener {
                 .concat(".jpg");
     }
 
+    private void startGame() {
+        String locale = Locale.getDefault().getLanguage();
+        setClickable(false);
+        restApi.getTranslation(locale, new Callback<Translation>() {
+            @Override
+            public void success(Translation translation, Response response) {
+                setButtonText(translation);
+                setNewPicture(idPainter);
+                setTranslation(translation);
+            }
+            @Override
+            public void failure(RetrofitError error) {
+                Log.e("ArtChallengeError", error.getMessage());
+            }
+        });
+        Log.i("ArtChallengeInfo", "Game start. " + "Language game: " + locale);
+    }
+
+    private void checkTruePainter(int numButtonAnswer) {
+        if (numButtonAnswer == numButtonTrueAnswer) {
+            countTrueAnswer ++;
+            Toast.makeText(this, translation.getRandGoodPhrases(), Toast.LENGTH_SHORT).show();
+            Log.i("ArtChallengeGame", "It's true answer");
+        } else {
+            countTrueAnswer = 0;
+            Toast.makeText(this, translation.getRandBadPhrases(), Toast.LENGTH_SHORT).show();
+            Log.i("ArtChallengeGame", "It's false answer");
+        }
+
+        progressBar.setProgress(countTrueAnswer);
+        if(countTrueAnswer != MAX_ANSWER) {
+            setButtonColorText(numButtonTrueAnswer, numButtonAnswer);
+            setButtonText(translation);
+            setNewPicture(idPainter);
+        } else {
+            Log.i("ArtChallengeGame", "You are WIN!!!");
+        }
+
+    }
+
+    private void setButtonText(Translation translation) {
+
+        ArrayList<Integer> uniqSet = RandomMath.randomSet(1, setPainters.size(), setButtons.size());
+        numButtonTrueAnswer = RandomMath.randomInt(1, 4);
+        idPainter = uniqSet.get(numButtonTrueAnswer - 1);
+
+        for (int i = 0; i < uniqSet.size(); i++) {
+            String textButton = translation.getPainters().get(uniqSet.get(i));
+            setButtons.get(i).setText(textButton);
+        }
+        Log.i("ArtChallengeAnswer", "True answer: " + translation.getPainters().get(idPainter)
+                + "; button number with the correct answer: " + numButtonTrueAnswer);
+    }
+
     private void setNewPicture(final int idPainter) {
-        Log.i("qwer", "sdf"+String.valueOf(idPainter));
         restApi.getPainter(idPainter, new Callback<Painter>() {
             @Override
             public void success(Painter painter, Response response) {
-
-                Log.i("qwer", "answer " + response.getBody().mimeType());
                 int pictureQuantity = painter.getPictureQuantity();
-                Log.i("qwer", "картин у художника " + String.valueOf(pictureQuantity));
-                String url = createUrlForPicture(idPainter, pictureQuantity);
-                Log.i("qwer", url);
+                Log.i("ArtChallengeInfo", "Download info about painter");
+                Log.i("ArtChallengeInfo", "ID painder: " + String.valueOf(idPainter)
+                        + "; Name painter: " + translation.getPainters().get(idPainter)
+                        + "; Quantity of paintings from the painters: " + String.valueOf(pictureQuantity));
+                picURl = createUrlForPicture(idPainter, pictureQuantity);
                 Picasso.with(GameActivity.this)
-                        .load(url)
+                        .load(picURl)
                         .memoryPolicy(MemoryPolicy.NO_CACHE, MemoryPolicy.NO_STORE)
                         .networkPolicy( NetworkPolicy.NO_STORE)
                         .into(imageViewPicture, new com.squareup.picasso.Callback() {
                             @Override
                             public void onSuccess() {
-
-                                resetBackgroundColor();
+                                resetButtonColorText();
                                 setClickable(true);
+                                Log.i("ArtChallengeInfo", "Download picture url: " + picURl);
                             }
 
                             @Override
                             public void onError() {
-
+                                Log.i("ArtChallengeError", "Error download picture");
                             }
                         });
 
@@ -229,14 +224,8 @@ public class GameActivity  extends Activity implements View.OnClickListener {
 
             @Override
             public void failure(RetrofitError error) {
-                Log.i("qwer", error.getMessage());
-//                Log.i("qwer",  String.valueOf(error.getResponse().getStatus()));
-                error.printStackTrace();
+                Log.i("ArtChallengeError", error.getMessage());
             }
         });
-    }
-
-    public void setTranslation(Translation translation) {
-        this.translation = translation;
     }
 }
